4caa289c364c454c2c8b80fbf83e34b2620395b1
Michael Suarez-Russell <emailformiko@gmail.com>
2026-01-16 17:29:24 -0800

    Initial Xcode project

---
+++ NomieTests/NomieTests.swift
@@ -0,0 +1,17 @@
+//
+//  NomieTests.swift
+//  NomieTests
+//
+//  Created by Michael Suarez-Russell on 1/16/26.
+//
+
+import Testing
+@testable import Nomie
+
+struct NomieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
